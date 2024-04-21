0737b10b31a5552873e7c0ea0a96a16e78fa3889
Jiahao9 <simjiahao9@gmail.com>
2024-04-21 19:57:43 +0900

    [UPD] delete unnecessary frame & add width range param

--- src/handle_estimator.cpp
+++ src/handle_estimator.cpp
@@ -10,41 +10,44 @@
 #include <pcl_ros/transforms.h>
 #include <tf2_ros/transform_broadcaster.h>
 #include <tf2_ros/buffer.h>
+#include <tf2_ros/transform_listener.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 
 #include "bag_handle_estimator/execute_ctrl.h"
 
 class BagHandleEstimator {
  private:
-  ros::NodeHandle    nh;
-  ros::Subscriber    sub_cloud;
-  ros::Subscriber    sub_ctrl;
-  ros::Publisher     pub_plane;
-  ros::ServiceServer service_execute_ctrl_;
-
+  ros::NodeHandle               nh;
+  ros::Subscriber               sub_cloud;
+  ros::Subscriber               sub_ctrl;
+  ros::Publisher                pub_plane;
+  ros::ServiceServer            service_execute_ctrl_;
   tf2_ros::Buffer               tfBuffer_;
   tf2_ros::TransformBroadcaster broadcaster;
+  tf2_ros::TransformListener    tfListener_;
 
   // paramater
   bool   execute_flag;
-  bool   estimated_flag;
   bool   publish_plane;
-  double depth_z_min;
-  double depth_z_max;
   double depth_x_min;
   double depth_x_max;
+  double depth_y_min;
+  double depth_y_max;
+  double depth_z_min;
+  double depth_z_max;
 
   std::string sub_point_topic_name;
   std::string sub_ctrl_topic_name;
   std::string camera_frame_name;
   std::string base_frame_name;
-  std::string map_frame_name;
 
-  geometry_msgs::PointStamped map_2_high_point;
+  geometry_msgs::PointStamped base_2_high_point;
 
  public:
-  BagHandleEstimator() {
-    this->estimated_flag = true;
+  BagHandleEstimator() : tfListener_(tfBuffer_) {
+  
+    // Initialize tfBuffer_ with a dedicated thread for populating data
+    tfBuffer_.setUsingDedicatedThread(true);
 
     // load rosparam
     ros::param::get("pub_plane_cloud", this->publish_plane);
@@ -52,15 +55,15 @@ class BagHandleEstimator {
 
     ros::param::get("depth_range_min_x", this->depth_x_min);
     ros::param::get("depth_range_max_x", this->depth_x_max);
+    ros::param::get("depth_range_min_y", this->depth_y_min);
+    ros::param::get("depth_range_max_y", this->depth_y_max);
     ros::param::get("depth_range_min_z", this->depth_z_min);
     ros::param::get("depth_range_max_z", this->depth_z_max);
 
     ros::param::get("sub_point_topic_name", this->sub_point_topic_name);
     ros::param::get("sub_ctrl_topic_name", this->sub_ctrl_topic_name);
 
-    ros::param::get("camera_frame_name", this->camera_frame_name);
     ros::param::get("base_frame_name", this->base_frame_name);
-    ros::param::get("map_frame_name", this->map_frame_name);
 
     // Create a ROS subscriber and publisher
     this->sub_cloud = nh.subscribe(this->sub_point_topic_name, 1, &BagHandleEstimator::cloud_cb, this);
@@ -72,7 +75,7 @@ class BagHandleEstimator {
   }  // bag_handle_estimator
 
   // execute control
-  bool execute_ctrl_server(bag_handle_estimator::execute_ctrl::Request&  req,
+  bool execute_ctrl_server(bag_handle_estimator::execute_ctrl::Request& req,
                            bag_handle_estimator::execute_ctrl::Response& res) {
     this->execute_flag = req.request;
     if (this->execute_flag == true) {
@@ -83,6 +86,8 @@ class BagHandleEstimator {
     res.response = true;
     return true;
   }
+
+  geometry_msgs::TransformStamped transformStamped;
 
   // callback function
   void cloud_cb(const sensor_msgs::PointCloud2ConstPtr& msg) {
@@ -99,14 +104,23 @@ class BagHandleEstimator {
 
     bool key = tfBuffer_.canTransform(this->base_frame_name, this->camera_frame_name, ros::Time(0), ros::Duration(2.0));
     if (key == false) {
-      ROS_ERROR("bag_handle_estimator : pcl canTransform failue");
+      ROS_WARN("bag_handle_estimator : pcl canTransform failue");
       return;
     }
-    
+
+    // Transform point cloud to base frame
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_transformed(new pcl::PointCloud<pcl::PointXYZ>);
+    try {
+        pcl_ros::transformPointCloud(this->base_frame_name, *cloud_input, *cloud_transformed, tfBuffer_);
+    } catch (tf2::TransformException& ex) {
+        ROS_WARN("[bag_handle_estimator] Failed to transform point cloud: %s", ex.what());
+        return;
+    }
+
     // filtering X limit
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_cut_x(new pcl::PointCloud<pcl::PointXYZ>);
     pcl::PassThrough<pcl::PointXYZ>     pass_x;
-    pass_x.setInputCloud(cloud_input);
+    pass_x.setInputCloud(cloud_transformed);
     pass_x.setFilterFieldName("x");
     pass_x.setFilterLimits(depth_x_min, depth_x_max);
     pass_x.filter(*cloud_cut_x);
@@ -114,12 +128,23 @@ class BagHandleEstimator {
       return;
     }
 
+    // filtering Y limit
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_cut_y(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PassThrough<pcl::PointXYZ>     pass_y;
+    pass_x.setInputCloud(cloud_cut_x);
+    pass_x.setFilterFieldName("y");
+    pass_x.setFilterLimits(depth_y_min, depth_y_max);
+    pass_x.filter(*cloud_cut_y);
+    if (cloud_cut_y->points.size() == 0) {
+      return;
+    }
+
     // filtering Z limit
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_cuted(new pcl::PointCloud<pcl::PointXYZ>);
     pcl::PassThrough<pcl::PointXYZ>     pass_z;
-    pass_z.setInputCloud(cloud_cut_x);
+    pass_z.setInputCloud(cloud_cut_y);
     pass_z.setFilterFieldName("z");
-    pass_z.setFilterLimits(depth_z_min, depth_z_max * 1.1);
+    pass_z.setFilterLimits(depth_z_min, depth_z_max);
     pass_z.filter(*cloud_cuted);
     if (cloud_cuted->points.size() == 0) {
       return;
@@ -150,26 +175,10 @@ class BagHandleEstimator {
       }
     }
 
-    // check if transformations are available
-    bool key2 = tfBuffer_.canTransform(this->map_frame_name, this->base_frame_name, ros::Time(0));
-    if (key2 == false) {
-      ROS_ERROR("bag_handle_estimator : canTransform failue");
-      return;
-    }
-
-    geometry_msgs::TransformStamped transformstamped;
-    transformstamped = tfBuffer_.lookupTransform(this->base_frame_name, this->map_frame_name, ros::Time(0));
-
-
     // prepare broadcast tf 
-    geometry_msgs::PointStamped base_2_high_point;
-    base_2_high_point.header.frame_id = this->base_frame_name;
-    base_2_high_point.header.stamp    = ros::Time(0);
-    base_2_high_point.point.x         = cloud_cuted->points[most_high_point_index].x ;
-    base_2_high_point.point.y         = cloud_cuted->points[most_high_point_index].y ;
-    base_2_high_point.point.z         = cloud_cuted->points[most_high_point_index].z - 0.04;
-    tf2::doTransform (base_2_high_point,this->map_2_high_point,transformstamped);
-    this->estimated_flag = true;
+    base_2_high_point.point.x = cloud_cuted->points[most_high_point_index].x ;
+    base_2_high_point.point.y = cloud_cuted->points[most_high_point_index].y ;
+    base_2_high_point.point.z = cloud_cuted->points[most_high_point_index].z - 0.04;
     broadcast_handle_point();
   }  // cloud_cb
 
@@ -188,18 +197,15 @@ class BagHandleEstimator {
 
   // broadcast tf
   void broadcast_handle_point() {
-    if (this->estimated_flag == false) {
-      return;
-    }
 
     geometry_msgs::TransformStamped transformStamped;
 
     transformStamped.header.stamp = ros::Time::now();
-    transformStamped.header.frame_id = this->map_frame_name;
+    transformStamped.header.frame_id = this->base_frame_name;
     transformStamped.child_frame_id = "handle_point";
-    transformStamped.transform.translation.x = map_2_high_point.point.x;
-    transformStamped.transform.translation.y = map_2_high_point.point.y;
-    transformStamped.transform.translation.z = map_2_high_point.point.z;
+    transformStamped.transform.translation.x = base_2_high_point.point.x;
+    transformStamped.transform.translation.y = base_2_high_point.point.y;
+    transformStamped.transform.translation.z = base_2_high_point.point.z;
     transformStamped.transform.rotation.x = 0.0;
     transformStamped.transform.rotation.y = 0.0;
     transformStamped.transform.rotation.z = 0.0;
